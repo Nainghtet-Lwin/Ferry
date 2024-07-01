bcce9c9736dc1bdeedb19189cb9f6fdbd38d7f11
Nainghtet-Lwin <96651876+Nainghtet-Lwin@users.noreply.github.com>
2024-07-01 20:44:18 +0630

    Add files via upload

---
+++ data/WIF_Office_4.js
@@ -0,0 +1,1 @@
+var json_WIF_Office_4 = {"type":"FeatureCollection","name":"WIF_Office_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"No":"22","Code":"Office","Name":"Office Location","Position":null,"Team":null,"Lat":16.84305,"Long":96.1706},"geometry":{"type":"Point","coordinates":[96.17059981,16.84305366]}}]}
